cf49a12d098bfff9c5a3a7d87ba50cf466063a22
Nick <nick@levo.financial>
2024-08-07 13:37:01 -0600

    support for chromadb

--- dsrag/vector_db.py
+++ dsrag/vector_db.py
@@ -6,6 +6,7 @@ import numpy as np
 import weaviate
 import weaviate.classes as wvc
 from weaviate.util import generate_uuid5
+import chromadb
 
 
 class VectorDB(ABC):
@@ -88,7 +89,7 @@ class BasicVectorDB(VectorDB):
 
     def search(self, query_vector, top_k=10):
         if not self.vectors:
-            return []
+            raise ValueError('No vectors stored in the database.')
         
         if self.use_faiss:
             return self.search_faiss(query_vector, top_k)
@@ -351,3 +352,76 @@ class WeaviateVectorDB(VectorDB):
     
     def delete(self):
         pass
+
+
+class ChromaDB(VectorDB):
+
+    def __init__(self, kb_id: str, storage_directory: str = '~/dsRAG'):
+        self.kb_id = kb_id
+        self.storage_directory = storage_directory
+        #self.vector_storage_path = os.path.join(self.storage_directory, 'vector_storage', f'{kb_id}.pkl')
+        self.client = chromadb.PersistentClient(path=storage_directory)
+        try:
+            self.collection = self.client.get_collection(kb_id)
+        except Exception as e:
+            if "does not exist" in str(e):
+                # Create the collection if it doesn't exists
+                self.collection = self.client.create_collection(kb_id, metadata={"hnsw:space": "cosine"})
+            else:
+                raise Exception(f"Error creating ChromaDB collection: {e}")
+    
+    def get_info(self):
+        return self.collection.count()
+        
+    
+    def add_vectors(self, vectors, metadata):
+        try:
+            assert len(vectors) == len(metadata)
+        except AssertionError:
+            raise ValueError('Error in add_vectors: the number of vectors and metadata items must be the same.')
+        
+        # Create the ids from the metadata, defined as {metadata["doc_id"]}_{metadata["chunk_index"]}
+        ids = [f"{meta['doc_id']}_{meta['chunk_index']}" for meta in metadata]
+
+        self.collection.add(
+            embeddings=vectors,
+            metadatas=metadata,
+            ids=ids
+        )
+    
+    def search(self, query_vector, top_k=10):
+        
+        query_results = self.collection.query(
+            query_embeddings=query_vector,
+            n_results=top_k,
+            include=["distances", "metadatas"]
+        )
+        
+        metadata = query_results["metadatas"][0]
+        distances = query_results["distances"][0]
+
+        results = []
+        for _, (distance, metadata) in enumerate(zip(distances, metadata)):
+            results.append({
+                'metadata': metadata,
+                'similarity': 1 - distance,
+            })
+        
+        results = sorted(results, key=lambda x: x['similarity'], reverse=True)
+
+        return results
+        
+    
+    def remove_document(self, doc_id):
+        data = self.collection.get(where={"doc_id": doc_id})
+        doc_ids = data["ids"]
+        self.collection.delete(ids=doc_ids)
+
+    def delete(self):
+        self.client.delete_collection(name=self.kb_id)
+    
+    def to_dict(self):
+        return {
+            **super().to_dict(),
+            'kb_id': self.kb_id,
+        }
