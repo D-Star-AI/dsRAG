3e2b0a3da403713e37e2c2618404110b04dcef4d
Nick <nick@levo.financial>
2024-08-08 10:52:22 -0600

    support for a SQLite Chunk DB

--- dsrag/chunk_db.py
+++ dsrag/chunk_db.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import os
 import pickle
+import sqlite3
 
 class ChunkDB(ABC):
     subclasses = {}
@@ -164,4 +165,132 @@ class BasicChunkDB(ChunkDB):
             **super().to_dict(),
             'kb_id': self.kb_id,
             'storage_directory': self.storage_directory,
+        }
+    
+
+class SQLiteDB(ChunkDB):
+
+    def __init__(self, kb_id: str, title: str = "", description: str = "", language: str = "en", db_path: str = '~/dsRAG'):
+        self.kb_id = kb_id
+        self.storage_directory = os.path.expanduser(db_path)
+        os.makedirs(os.path.join(self.storage_directory, 'chunk_storage'), exist_ok=True)
+        self.db_path = os.path.join(self.storage_directory, 'chunk_storage')
+
+        # Make sure main knowledge bases table exists
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        result = c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='knowledge_bases'")
+        if not result.fetchone():
+            c.execute("CREATE TABLE knowledge_bases (kb_id VARCHAR(256) PRIMARY KEY, title VARCHAR(256), description TEXT, language VARCHAR(16))")
+            conn.commit()
+        conn.close()
+
+        # Create a table for this kb_id if it doesn't exist
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        result = c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{kb_id}'")
+        if not result.fetchone():
+            # Insert this kb into the table
+            c.execute("INSERT INTO knowledge_bases (kb_id, title, description, language) VALUES (?, ?, ?, ?)", (kb_id, title, description, language))
+            c.execute(f"CREATE TABLE {kb_id} (doc_id VARCHAR(256), document_title VARCHAR(256), document_summary TEXT, section_title VARCHAR(256), section_summary TEXT, chunk_text TEXT, chunk_index INT)")
+            conn.commit()
+        conn.close()
+        
+
+    def add_document(self, doc_id: str, chunks: dict[dict]):
+        # Add the docs to the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        # Get the data from the dictionary
+        for chunk_index, chunk in chunks.items():
+            document_title = chunk.get('document_title', "")
+            document_summary = chunk.get('document_summary', "")
+            section_title = chunk.get('section_title', "")
+            section_summary = chunk.get('section_summary', "")
+            chunk_text = chunk.get('chunk_text', "")
+            c.execute(f"INSERT INTO {self.kb_id} (doc_id, document_title, document_summary, section_title, section_summary, chunk_text, chunk_index) VALUES (?, ?, ?, ?, ?, ?, ?)", (doc_id, document_title, document_summary, section_title, section_summary, chunk_text, chunk_index))
+
+        conn.commit()
+        conn.close()
+
+    def remove_document(self, doc_id: str):
+        # Remove the docs from the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        c.execute(f"DELETE FROM {self.kb_id} WHERE doc_id='{doc_id}'")
+        conn.commit()
+        conn.close()
+
+    def get_chunk_text(self, doc_id: str, chunk_index: int) -> str:
+        # Retrieve the chunk text from the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        c.execute(f"SELECT chunk_text FROM {self.kb_id} WHERE doc_id='{doc_id}' AND chunk_index={chunk_index}")
+        result = c.fetchone()
+        conn.close()
+        if result:
+            return result[0]
+        return ""
+
+    def get_document_title(self, doc_id: str, chunk_index: int) -> str:
+        # Retrieve the document title from the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        c.execute(f"SELECT document_title FROM {self.kb_id} WHERE doc_id='{doc_id}' AND chunk_index={chunk_index}")
+        result = c.fetchone()
+        conn.close()
+        if result:
+            return result[0]
+        return ""
+
+    def get_document_summary(self, doc_id: str, chunk_index: int) -> str:
+        # Retrieve the document summary from the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        c.execute(f"SELECT document_summary FROM {self.kb_id} WHERE doc_id='{doc_id}' AND chunk_index={chunk_index}")
+        result = c.fetchone()
+        conn.close()
+        if result:
+            return result[0]
+        return ""
+
+    def get_section_title(self, doc_id: str, chunk_index: int) -> str:
+        # Retrieve the section title from the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        c.execute(f"SELECT section_title FROM {self.kb_id} WHERE doc_id='{doc_id}' AND chunk_index={chunk_index}")
+        result = c.fetchone()
+        conn.close()
+        if result:
+            return result[0]
+        return ""
+
+    def get_section_summary(self, doc_id: str, chunk_index: int) -> str:
+        # Retrieve the section summary from the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        c.execute(f"SELECT section_summary FROM {self.kb_id} WHERE doc_id='{doc_id}' AND chunk_index={chunk_index}")
+        result = c.fetchone()
+        conn.close()
+        if result:
+            return result[0]
+        return ""
+
+    def get_all_doc_ids(self) -> list:
+        # Retrieve all document IDs from the sqlite table
+        conn = sqlite3.connect(os.path.join(self.db_path, 'dsRAG.db'))
+        c = conn.cursor()
+        c.execute(f"SELECT DISTINCT doc_id FROM {self.kb_id}")
+        results = c.fetchall()
+        conn.close()
+        return [result[0] for result in results]
+
+    def to_dict(self):
+        return {
+            **super().to_dict(),
+            'kb_id': self.kb_id,
+            'db_path': self.db_path,
+            'title': self.title,
+            'description': self.description,
+            'language': self.language,
         }
