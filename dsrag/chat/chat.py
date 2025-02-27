67c61926d961f7d173f1144b5bb0ce60d0578e5a
Zach <zacharymccormick755@gmail.com>
2025-02-27 10:01:35 -0700

    SQLite ChatThreadDB bug fixes

--- dsrag/chat/chat.py
+++ dsrag/chat/chat.py
@@ -323,8 +323,6 @@ def _get_chat_response(
         # run searches
         search_results = {}
         for kb_id, queries in search_queries_by_kb.items():
-            print (f"Running search for KB: {kb_id}")
-            print (f"Queries: {queries}")
             kb = kbs.get(kb_id)
             search_results[kb_id] = kb.query(search_queries=queries, rse_params=rse_params, metadata_filter=metadata_filter)
 
