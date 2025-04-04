37c5de0b08924f5dad3d894fc6a8f480cc12dffe
Zach <zacharymccormick755@gmail.com>
2025-04-04 11:00:08 -0600

    add new gemini models

--- dsrag/utils/model_names.py
+++ dsrag/utils/model_names.py
@@ -13,8 +13,11 @@ GEMINI_MODEL_NAMES = [
     "gemini-1.5-flash-002",
     "gemini-1.5-pro-002", 
     "gemini-2.0-flash",
+    "gemini-2.0-flash-lite",
     "gemini-2.0-flash-thinking-exp",
     "gemini-2.0-pro-exp",
+    "gemini-2.5-pro-exp-03-25",
+    "gemini-2.5-pro-preview-03-25", 
 ]
 
 OPENAI_MODEL_NAMES = [
